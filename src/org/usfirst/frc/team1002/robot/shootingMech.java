fea649657d0c3fca1322f5a57959c969ee70ac1d
NathanPendergrast <nathanpendergrast@yahoo.com>
2018-11-19 02:54:15 -0500

    this better be the last time i do this

--- src/org/usfirst/frc/team1002/robot/shootingMech.java
+++ src/org/usfirst/frc/team1002/robot/shootingMech.java
@@ -1,19 +1,19 @@
 package org.usfirst.frc.team1002.robot;
 
 //import edu.wpi.first.wpilibj.TalonSRX;
-import com.ctre.CANTalon;
+//import com.ctre.CANTalon;
 public class shootingMech {
 
 	private static final int SHOOTERCONTROL = 2;
-	CANTalon motor;
+//	CANTalon motor;
 
 	void init() {
-		motor = new CANTalon(SHOOTERCONTROL);
+//		motor = new CANTalon(SHOOTERCONTROL);
 	}
 
 	void teleOp() {
 		if (Robot.operator.getRawAxis(Xbox.AXIS_RTRIGGER) > 0) {
-			motor.set(1);
+//			motor.set(1);
 		}
 	}
 }
